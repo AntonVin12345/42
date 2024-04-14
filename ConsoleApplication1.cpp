2bdeec8be476bee885f50a64ad2b5539c47ec3f4
AntonVin12345 <167004869+AntonVin12345@users.noreply.github.com>
2024-04-14 16:44:04 +0300

    Первая версия проекта

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void drawTriangle(int n) {
+    for (int i = 0; i < n; i++) {
+       
+        for (int j = 0; j < n - i - 1; j++) {
+            std::cout << " ";
+        }
+        
+        for (int k = 0; k < 2 * i + 1; k++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int height;
+    std::cout << " ";
+    std::cin >> height;
+
+    drawTriangle(height);
+
+    return 0;
+}
